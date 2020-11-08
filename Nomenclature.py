584ef8df5f828f874499e759761e48a242114753
Ahmed HISHAM [Autocool] <ahmed.hisham@Autocool.local>
2020-11-08 16:46:46 +0200

    add fitting nomenclature, fix names by converting the  _ into spaces

--- Nomenclature.py
+++ Nomenclature.py
@@ -202,14 +202,25 @@ Nomenclatures['Parallel Flow Coil'] = {
 }
 
 Nomenclatures['Fitting'] = {
-    'n': 2,
+    'n': 7,
     'ID': 'Fitting ',
     'Parameters': {
         'Type': {
             'n': 1,
             'list': {
                 'Connector': 'Connector',
-                'Swivel_connector': 'Swivel connector'
+                'Swivel_connector': 'Swivel connector',
+                'Swivel_Nut': 'Swivel nut',
+                'Crimp_Racor': 'Crimp racor',
+                'Reusable_Racor': 'Reusable racor',
+                'Clamp_racor': 'Clamp racor (drain hose)',
+                'Adaptor': 'Adaptor',
+                'Nipple': 'Nipple',
+                'Tee': 'Tee (Threaded)',
+                'T_racor': 'T racor',
+                'Cap': 'Cap',
+                'Plug': 'Plug',
+                'Nut': 'Nut'
             },
             'sep': '-',
             'ID': 'Fitting_type'
@@ -218,10 +229,64 @@ Nomenclatures['Fitting'] = {
             'n': 2,
             'list': {
                 'Oring': 'Oring',
-                'Flare _SAE45': 'Flare SAE 45'
+                'Face_Oring': 'Face Oring',
+                'Flare_SAE45': 'Flare SAE45',
+                'Flare_JIC37': 'Flare JIC37',
+                'Clamp': 'Clamp'
             },
             'sep': '-',
             'ID': 'Fitting_Seal'
+        },
+        'Material': {
+            'n': 3,
+            'list': {
+                'Al': 'Al',
+                'Brass': 'Brass',
+                'GSt': 'G.St.'
+            },
+            'sep': '-',
+            'ID': 'Fitting_Material'
+        },
+        'Connection 1': {
+            'n': 4,
+            'list': {
+                '5/8inch_18UNF': '5/8inch 18UNF',
+                '3/4inch_16UNF': '3/4inch 16UNF',
+                '7/8inch_14UNF': '7/8inch 14UNF',
+                '1_1/16inch_14UNF': '1 1/16inch 14UNF',
+                '1_1/8inch_12UNF': '1 1/8inch 12UNF',
+                'M30x2': 'M30x2',
+                '': 'Blank (if T racor,)'
+            },
+            'sep': '-',
+            'ID': 'Fitting_conn1'
+        },
+        'Connection 2': {
+            'n': 5,
+            'list': {
+                'Counterbored': 'Counterbored (connector)',
+                'Counterbored_neck': 'Counterbored neck (connector)',
+                'Neck': 'Neck (connector)',
+                'Tube': 'Tube (Swivel Nut/connector)',
+                'Hose_size': 'Hose (crimp/reusable/clamp racor)',
+                '': 'Thread (adaptor/nipple)',
+                '2x': '2xThread (Tee)',
+                '': 'blank (cap/nut/T racor)'
+            },
+            'sep': ' ',
+            'ID': 'Fitting_conn2'
+        },
+        'Conn 2 Dimension': {
+            'n': 6,
+            'list': '',
+            'sep': ' (',
+            'ID': 'Fitting_conn_dim'
+        },
+        'Comment, if any': {
+            'n': 7,
+            'list': '',
+            'sep': ')',
+            'ID': 'Fitting_Comment'
         }
     }
 }
